b365e7051e794e6a547bb4200827926aeea9b45c
KazantsevaJane <103194481+KazantsevaJane@users.noreply.github.com>
2022-12-31 12:01:40 +0500

    обавила документацию

--- main.py
+++ main.py
@@ -11,7 +11,16 @@ import matplotlib.pyplot as plt
 
 
 class UsersInput:
+    """
+    Класс пользовательского ввода
+    Attributes:
+        file_name: название введённого пользователем файла
+        profession_name: названием введённой пользователем профессии
+    """
     def __init__(self):
+        """
+        Инициализирует объект класса, вызывает методы проверки корректности введеных данных
+        """
         self.file_name = input('Введите название файла: ')
         self.profession_name = input('Введите название профессии: ')
 
@@ -21,6 +30,16 @@ class UsersInput:
     if __name__ == '__main__':
         @staticmethod
         def check_file_name(file_name):
+            """
+            Проверяет имя файла на корректность
+            Корректное название файла не должно быть пустым и должно соджержать расширение файла
+            Args:
+                file_name (str): имя файла
+
+            Returns:
+                file_name: string
+
+            """
             if file_name == '' or '.' not in file_name:
                 print('Некорректное название файла, попробуйте ещё раз!')
                 sys.exit()
@@ -28,6 +47,15 @@ class UsersInput:
 
         @staticmethod
         def check_profession_name(profession_name):
+            """
+            Проверка введённой пользователем профессии на корректность.
+            Корректное название профессии не должно быть пустым
+            Args:
+                profession_name (str): введённое пользователем название профессии
+
+            Returns:
+                profession_name: string
+            """
             if profession_name == '':
                 print('Некорректное название профессии')
                 sys.exit()
@@ -35,7 +63,21 @@ class UsersInput:
 
 
 class DataSet:
+    """
+    Класс, отвечающий за данные о вакансиях. Данные получены из csv-файла
+    Attributes:
+        reader: информация, считанная из csv-файла в виде массива строк таблицы
+        (внешний массив - строки, внутренние массивы - информация из стобцов в данной строке)
+        columns_names: названия столбцов в csv-файле
+        vacancies_data: массив обработанных данных по вакансиям из csv-файла
+        (в него попадают только строки, в которых заполнены все столбцы)
+    """
     def __init__(self, file_name):
+        """
+        Инициализирует объект DataSet, обрабатывает данные из csv-файла
+        Args:
+            file_name (str): имя csv-файла,на основе которого собираются данные по вакансиям
+        """
         self.reader = [row for row in csv.reader(open(file_name, encoding='utf_8_sig'))]
         if len(self.reader) == 0:
             print('Пустой файл')
@@ -49,6 +91,17 @@ class DataSet:
 
 
 class Vacancy:
+    """
+    Класс, отвечающий за представление вакансии
+    Attributes:
+        name: наименование вакансии
+        salary_from: нижняя граница заработной платы
+        salary_to: верхняя граница заработной платы
+        salary_currency: валюта заработной платы
+        area_name: название области деятельности
+        published_at: дата публикации вакансии
+        salary: стандартное значение заработной платы
+    """
     name: str
     salary_from: int or float
     salary_to: int or float
@@ -58,12 +111,22 @@ class Vacancy:
     salary: str
 
     def __init__(self, vacancy):
-
+        """
+        Инициализирует объект Vacancy
+        Args:
+            vacancy (dict): словарь, содержащий данные для инициализируемого объекта Vacancy
+        """
         for key, value in vacancy.items():
             self.__setattr__(key, self.formatter(key, value))
 
     @staticmethod
     def formatter(key, value):
+        """
+        Приводит к нужному типу значения атрибутов данного объекта
+        Args:
+            key: ключ
+            value: значение
+        """
         if key in ['salary_from', 'salary_to']:
             return float(value)
         if key == 'published_at':
@@ -72,36 +135,81 @@ class Vacancy:
 
 
 class Salary:
+    """
+    Класс для представления заработной платы
+    Attributes:
+        salary_from: нижняя граница заработной платы
+        salary_to: верхняя граница заработной платы
+        salary_currency: валюта заработной платы
+    """
     def __init__(self, salary_from, salary_to, salary_currency):
+        """
+        Инициализирует объект класса Salary
+        Args:
+            salary_from: нижняя граница заработной платы
+            salary_to: верхняя граница заработной платы
+            salary_currency: валюта заработной платы
+        """
         self.salary_from = salary_from
         self.salary_to = salary_to
         self.salary_currency = salary_currency
 
 
 class SalaryDict:
+    """
+    Вспомогательный класс для обработки информации о зарплате
+    Attributes:
+        salary_dict: данные о всех существующих на сайте зарплатах для заданного key (в качестве ключей выступают города)
+        __aver_salary_dict: словарь с данными о средней зарплате для каждого key из salary_dict
+    """
     def __init__(self):
+        """Инициализирует объект класса SalaryDict"""
         self.salary_dict = {}
         self.__aver_salary_dict = {}
 
     def add_salary(self, key, salary):
+        """
+        Добавляет salary в список зарплат по заданному key или инициализирует список зарплат для данного key
+        Args:
+            key: ключ, по которому нужно добавить salary в словарь salary_dict
+            salary: значение добавляемой в словарь зарплаты
+
+        """
         if self.salary_dict.get(key) is None:
             self.salary_dict[key] = []
         return self.salary_dict[key].append(salary)
 
     def get_aver_salary(self):
+        """Возвращает словарь с данными о средней зарплате для каждого key из salary_dict"""
         for key, value in self.salary_dict.items():
             self.__aver_salary_dict[key] = int(mean(value))
         return self.__aver_salary_dict
 
 
 class CountDict:
+    """
+    Класс для подсчёта количества любых данных для разных городов
+    Attributes:
+        length: количество ключей в count_dict
+        count_dict: словарь с данными о количестве - value для заданного города - key
+        big_cities: города, в которых количество данных для данного города больше или равно 10% от общего количества
+        данных по всем городам
+        top_proportion_dict: словарь, отсортированный в порядке убывания отношения колчиества данных для данного города
+        к общему количеству данных по всем городам
+    """
     def __init__(self):
+        """Инициализирует объект класса CountDict"""
         self.length = 0
         self.count_dict = {}
         self.big_cities = []
         self.top_proportion_dict = {}
 
     def add(self, key):
+        """
+        Увеличивает на единицу общее количество данных и количество данных для заданного города
+        Args:
+            key: название заданного города
+        """
         if self.count_dict.get(key) is None:
             self.count_dict[key] = 0
         self.count_dict[key] += 1
@@ -109,6 +217,7 @@ class CountDict:
         return
 
     def get_proportion(self):
+        """Обновляет top_proportion_dict для заданного объекта CountDict"""
         proportion_dict = {}
         for key, value in self.count_dict.items():
             proportion = value / self.length
@@ -121,6 +230,18 @@ class CountDict:
 
 
 class ParseData:
+    """
+    Класс для представления данных о зарплате и вакансиях, полученных из csv-файла
+    Attributes:
+        currency_to_rub: словарь для перевода любой валюты в рубли, key - название валюты, value - значение в рублях
+        денежной единицы
+        salary_lvl_by_year: уровень зарплаты по всем профессиям в этом году
+        count_vac_by_year: общее количество опубликованных вакансий в этом году
+        salary_lvl_by_year_for_prof: уровень зарплаты в данной профессии в этом году
+        count_vac_by_year_for_prof: количество опубликованных вакансий с данной профессией в этом году
+        salary_lvl_by_city: уровень зарплаты по городу
+        vacancy_rate_by_city: уровень вакансий по городу
+    """
     currency_to_rub = {'AZN': 35.68,
                        'BYR': 23.91,
                        'EUR': 59.90,
@@ -133,6 +254,7 @@ class ParseData:
                        'UZS': 0.0055}
 
     def __init__(self):
+        """Инициализирует объект класса ParseData"""
         self.salary_lvl_by_year = SalaryDict()
         self.count_vac_by_year = CountDict()
         self.salary_lvl_by_year_for_prof = SalaryDict()
@@ -141,6 +263,13 @@ class ParseData:
         self.vacancy_rate_by_city = CountDict()
 
     def get_data(self, vacancies, prof):
+        """
+        Возвращает обновлённые значения всех аттрибутов объекта class ParseData
+
+        Args:
+            vacancies: список объектов типа Vacancy, содержащий данные о вакансиях
+            prof: профессия, для которой мы обновляем count_vac_by_year_for_prof и salary_lvl_by_year_for_prof
+        """
         self.inspection_vacancy(prof, vacancies)
         self.checked_salary()
         self.salary_lvl_by_city, list_del_town = self.get_top_aver_salary(self.salary_lvl_by_city)
@@ -152,6 +281,8 @@ class ParseData:
                self.salary_lvl_by_city, self.vacancy_rate_by_city
 
     def checked_salary(self):
+        """Проверяет, совпадает ли количество данных по вакансиям и зарплатам
+         в атрибутах текущего объекта и, если нет, добавляет недостающие"""
         if self.salary_lvl_by_year_for_prof.salary_dict == {}:
             self.salary_lvl_by_year_for_prof.salary_dict = {x: [0] for x in self.salary_lvl_by_year.salary_dict.keys()}
         elif self.salary_lvl_by_year_for_prof.salary_dict != {} and len(
@@ -170,6 +301,15 @@ class ParseData:
                     self.count_vac_by_year_for_prof.count_dict[key] = 0
 
     def inspection_vacancy(self, prof, vacancies):
+        """
+        Вносит информацию о вакансиях и переданной профессии в атрибуты текущего объекта
+        Args:
+            prof: профессия, для которой мы обновляем count_vac_by_year_for_prof и salary_lvl_by_year_for_prof
+            vacancies: список объектов типа Vacancy, содержащий данные о вакансиях
+
+        Returns:
+
+        """
         for vacancy in vacancies:
             vacancy_salary = (vacancy.salary_from + vacancy.salary_to) / 2 * ParseData.currency_to_rub[vacancy.salary_currency]
             self.salary_lvl_by_year.add_salary(vacancy.published_at, vacancy_salary)
@@ -182,6 +322,11 @@ class ParseData:
 
     @staticmethod
     def get_top_aver_salary(list_all_salary):
+        """
+        Возвращает топ-10 городов с самым высоким значением средней зарплаты по городу и список городов,
+        Args:
+            list_all_salary: данные о зарплатах для каждого города
+        """
         dic_average = []
         dic_town_count = {}
         for i in range(len(list_all_salary.salary_dict)):
@@ -214,6 +359,11 @@ class ParseData:
 
     @staticmethod
     def get_top_rate_by_city(vacancy_rate_by_city):
+        """
+        Возвращает топ-10 городов с самым большим количеством вакансий
+        Args:
+            vacancy_rate_by_city: объект CountDict с данными о количестве вакансий для каждого города
+        """
         s = vacancy_rate_by_city.length
         list_del_town = []
         for i in reversed(range(len(list_del_town))):
@@ -233,7 +383,20 @@ class ParseData:
 
 
 class Report:
+    """
+    Класс для представления статистики в виде xlsx-документа
+    Attributes:
+        wb: контейнер для всех частей xlsx-документа
+        sheet1: текущий активный xlsx-документа лист, отвечающий за статистику по годаи
+        sheet2: дополнительный лист для статистики по городам
+        fig: контейнер, содержащий области для графиков
+        ax1: область для графика, отображающего уровень зарплат по годам
+        ax2: область для графика, отображающего количество вакансий по годам
+        ax3: область для графика, отображающего уровень зарплат по городам
+        ax4: область для графика, отображающего количество вакансий по городам
+    """
     def __init__(self):
+        """Инициализирует объект класса Report"""
         self.wb = Workbook()
         self.sheet1 = self.wb.active
         self.sheet1.title = 'Статистика по годам'
@@ -250,6 +413,12 @@ class Report:
         self.ax4.set_title('Доля вакансий по городам')
 
     def generate_excel(self, data, prof):
+        """
+        Заполняет данными листы sheet1 и sheet2
+        Args:
+            data: данные
+            prof: название профессии
+        """
         salary_lvl_by_year = data[0]
         count_vac_by_year = data[1]
         salary_lvl_by_year_for_prof = data[2]
@@ -289,11 +458,15 @@ class Report:
 
     @staticmethod
     def set_border(ws, side):
+        """Устанавливает границу таблицы"""
         for cell in ws._cells.values():
             cell.border = Border(top=side, bottom=side, left=side, right=side)
 
     @staticmethod
     def column_width(ws):
+        """
+        Расчитывает ширину стлбцов
+        """
         dims = {}
         for row in ws.rows:
             for cell in row:
@@ -303,6 +476,7 @@ class Report:
             ws.column_dimensions[col].width = value + 2
 
     def generate_image(self, data, prof):
+        """Строит графики по данным"""
         count_vac_by_year, count_vac_by_year_for_prof, salary_lvl_by_city, salary_lvl_by_year, salary_lvl_by_year_for_prof, vacancy_rate_by_city, width_12, x_list1_1, x_list1_2, x_nums_1 = self.calculation(
             data)
 
@@ -352,6 +526,7 @@ class Report:
         plt.savefig('graph.png')
 
     def search_hyphens(self, salary_lvl_by_city):
+        """Ищет дефисы в таблице и добавляет к ним символ переноса строки"""
         list_names = {}
         for key, value in salary_lvl_by_city.items():
             if ' ' in key:
@@ -378,6 +553,7 @@ class Report:
 
 
 def output(data_vacancies, profession_name):
+    """Вывод данных в консоль"""
     all_data_vacancies = []
     for data_vacancy in data_vacancies:
         data_vacancy = Vacancy(dict(zip(column_names, data_vacancy)))
